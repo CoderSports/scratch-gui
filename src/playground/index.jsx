dcfc20e91f3dcd3e036b656f037a3d333ea7525c
Griffin Barrett <griffin-barrett@outlook.com>
2021-07-04 14:51:27 -0400

    removed google analitics

--- src/playground/index.jsx
+++ src/playground/index.jsx
@@ -7,7 +7,6 @@ import 'intl'; // For Safari 9
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import analytics from '../lib/analytics';
 import AppStateHOC from '../lib/app-state-hoc.jsx';
 import BrowserModalComponent from '../components/browser-modal/browser-modal.jsx';
 import supportedBrowser from '../lib/supported-browser';
@@ -15,7 +14,6 @@ import supportedBrowser from '../lib/supported-browser';
 import styles from './index.css';
 
 // Register "base" page view
-analytics.pageview('/');
 
 const appTarget = document.createElement('div');
 appTarget.className = styles.app;
